a659e76697e045c7b14be9d9d12ca812cb65c69a
Camilo Mendoza <ca.mendoza968@uniandes.edu.co>
2018-03-08 18:40:58 -0500

    Added correct calls to Ekeko/x and search template selection

--- src/archtoring/handlers/OpenHandler.java
+++ src/archtoring/handlers/OpenHandler.java
@@ -16,28 +16,38 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 import damp.ekeko.EkekoNature;
 import damp.ekeko.snippets.data.TemplateGroup;
-import damp.ekeko.soot.SootNature;
+import damp.ekeko.snippets.gui.TransformationEditor;
 import damp.util.Natures;
-public class OpenHandler extends AbstractHandler{
+
+public class OpenHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		//get selected file
+		// get selected project
 		IStructuredSelection selectedStructure = HandlerUtil.getCurrentStructuredSelection(event);
-		//IFile file = (IFile) selectedStructure.getFirstElement();
-		//IPath path = file.getLocation();
-		//open file dialog
+		// open file dialog
 		Shell shell = HandlerUtil.getActiveShell(event);
 		FileDialog dialog = new FileDialog(shell);
 		dialog.setText("Select Search Template");
-		dialog.open();
-		//create template using selected file
-		selectForEkeko(selectedStructure);
-		TemplateGroup template = TemplateGroup.newFromClojureGroup("C:\\Users\\Asistente\\Desktop\\EkekoToAcademia\\TemplatesEkekoX\\FindDoPost-MatchSet2.ekx");
-		System.out.println(template.getResults());
+		String[] extensions = { "*.ekx" };
+		dialog.setFilterExtensions(extensions);
+		if (dialog.open() != null) {
+			String filename = dialog.getFileName();
+			if (filename == "")
+				System.out.println("You didn't select any file.");
+			else
+				System.out.println("You chose " + dialog.getFilterPath() + "\\" + filename);
+			// create template using selected file
+			selectForEkeko(selectedStructure);
+			TransformationEditor editor = new TransformationEditor();
+			Object myTransformation = TransformationEditor.deserializeClojureTransformation(dialog.getFilterPath() + "\\" + filename);
+			Object mySearchTemplate = editor.getLHSOfTransformation(myTransformation);
+			TemplateGroup lhsTemplateGroup = TemplateGroup.newFromClojureGroup(mySearchTemplate);
+			System.out.println("Resultados:" + lhsTemplateGroup.getResults());
+		}
 		return null;
 	}
-	
+
 	/*
 	 * Include selection in Ekeko queries
 	 */
@@ -46,18 +56,18 @@ public class OpenHandler extends AbstractHandler{
 			for (Iterator<?> it = ((IStructuredSelection) selection).iterator(); it.hasNext();) {
 				Object element = it.next();
 				IProject project = null;
-				if (element instanceof IProject) 
+				if (element instanceof IProject)
 					project = (IProject) element;
-				else if (element instanceof IAdaptable) 
+				else if (element instanceof IAdaptable)
 					project = (IProject) ((IAdaptable) element).getAdapter(IProject.class);
 				if (project != null) {
 					try {
-						//enable Ekeko nature if disabled
-						if(project.hasNature(EkekoNature.NATURE_ID)) {
-							//remove Soot nature first, because it requires Ekeko nature
-							if(project.hasNature(SootNature.NATURE_ID))
-								Natures.removeNature(project, SootNature.NATURE_ID);
-							Natures.removeNature(project, EkekoNature.NATURE_ID);
+						// enable Ekeko nature if disabled
+						if (project.hasNature(EkekoNature.NATURE_ID)) {
+							// remove Soot nature first, because it requires Ekeko nature
+							// if(project.hasNature(SootNature.NATURE_ID))
+							// Natures.removeNature(project, SootNature.NATURE_ID);
+							// Natures.removeNature(project, EkekoNature.NATURE_ID);
 						} else
 							Natures.addNature(project, EkekoNature.NATURE_ID);
 					} catch (CoreException e) {
